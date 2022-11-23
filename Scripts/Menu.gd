93863f36e9ca807881a7e07e98a359b2653d4f5c
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2022-11-23 17:25:19 +1030

    e

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -6,7 +6,7 @@ func _process(delta):
 	$Cursor.visible = Input.get_mouse_mode() == 1
 	timer += delta
 	$Title.rect_position.x = 252 + sin(timer*2) * 10
-	$Play.rect_rotation = sin(timer*1.25) * 10
+	$Buttons.rect_rotation = sin(timer*1.25) * 10
 	$Cursor.position = get_global_mouse_position()
 	
 func _ready():
