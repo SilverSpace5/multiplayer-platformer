fb82bfbc8046b3b03d19f25229d09a02c1912120
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2022-11-24 18:02:51 +1030

    e

--- Scripts/Autoload/Global.gd
+++ Scripts/Autoload/Global.gd
@@ -15,7 +15,7 @@ var textures = []
 # 0 = Blue Guy, 1 = Tim
 
 func _ready():
-	for character in ["blue-guy", "tim", "Phil"]:
+	for character in ["blue-guy2", "tim", "Phil"]:
 		textures.append(load("res://Assets/Players/" + character + ".png"))
 	
 	# Gets the current scene
