6e53cb9e16c50620f38801f6f6b5a39267f079ac
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2022-12-07 08:09:37 +1030

    e

--- Scripts/Scenes/game.gd
+++ Scripts/Scenes/game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var timer = 0
+
 func _ready():
 	global.sceneName = "Game"
 	yield(get_tree().create_timer(0.1), "timeout")
@@ -15,6 +17,26 @@ func _ready():
 func _process(delta):
 	if global.player:
 		$Camera2D.position += (global.player.position-$Camera2D.position)/5
+	timer += delta
+	if timer < 0.5:
+		return
+	var names = []
+	for player in $Players.get_children():
+		names.append(player.name)
+	for player in network.playerData:
+		if not player in names:
+			if network.playerData[player].has("scene"):
+				if network.playerData[player]["scene"] == "Game":
+					var player2 = load("res://Player.tscn").instance()
+					$Players.add_child(player2)
+					player2.position = Vector2(100, 125)
+					player2.id = player
+					player2.name = player
+		else:
+			if network.playerData[player].has("scene"):
+				if network.playerData[player]["scene"] != "Game":
+					print(network.playerData[player]["scene"])
+					$Players.get_node(player).queue_free()
 
 func _on_Menu_pressed():
 	global.player = null
